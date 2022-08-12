fa26355240b1f15429e7a594dcd949e687cd0fe2
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-12 23:17:58 +0200

    Displaying advice.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const button = document.querySelector("#next-advice");
+const numberSpan = document.querySelector("#advice-number");
+const advicePara = document.querySelector("#advice-content");
 
 const URL = "https://api.adviceslip.com/advice";
 
 const generateAdvice = async () => {
   const response = await fetch(URL);
   const data = await response.json();
-  console.log(data);
+
+  numberSpan.textContent = data.slip.id;
+  advicePara.textContent = data.slip.advice;
 };
 
 button.addEventListener("click", generateAdvice);
