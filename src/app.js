60b420a462e3fdd8b5ed6766c3c57c484db658dc
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-12 23:02:10 +0200

    Added simple button reaction.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-alert("working");
+const button = document.querySelector("#next-advice");
+
+const URL = "https://api.adviceslip.com/advice";
+
+const generateAdvice = () => {
+  alert("Lol");
+};
+
+button.addEventListener("click", generateAdvice);
