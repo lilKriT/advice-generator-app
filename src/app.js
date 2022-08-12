88496f53724abb84ec1415edd3ce07cc4f667c07
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-12 23:12:23 +0200

    Fetching the data.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const button = document.querySelector("#next-advice");
 
 const URL = "https://api.adviceslip.com/advice";
 
-const generateAdvice = () => {
-  alert("Lol");
+const generateAdvice = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
 };
 
 button.addEventListener("click", generateAdvice);
